8eec0d735cb8e1b166391999bbebfad65aca5fa8
Jibrilos <webxite23@gmail.com>
2021-04-12 23:21:55 +0300

    Added Appbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import Home from "./Home"
-import Todo from "./Todo"
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./Navbar";
 
-function App(props) {
-    return (
-      <div className="App">
-      
-      </div>
-    );
+function App() {
+  const [todo, setTodo] = useState({ description: "", date: "" });
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
+
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+  };
+
+  return (
+    <div className="App">
+        <Navbar/>
+      <input
+        placeholder="Description"
+        name="description"
+        value={todo.description}
+        onChange={inputChanged}
+      />
+      <input
+        placeholder="Date"
+        name="date"
+        value={todo.date}
+        onChange={inputChanged}
+      />
+      <button onClick={addTodo}>Add</button>
+    </div>
+  );
 }
 
 export default App;
