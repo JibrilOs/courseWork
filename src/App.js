78174176ceed1bba02451ed81a9fb8367d017df4
Jibrilos <webxite23@gmail.com>
2021-04-12 23:13:04 +0300

    deleted soem components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import {BrowserRouter as Router,Link,Route,Switch} from "react-router-dom";
 import Home from "./Home"
 import Todo from "./Todo"
 
 function App(props) {
     return (
       <div className="App">
-        <Router>
-          <div>
-            <Link to="/" style={{ marginRight:"10px" }}>
-              Home
-            </Link>
-            <Link to="/Todo">TodoList</Link>
-            <Switch>
-              <Route path="/" exact>
-                <Home />
-              </Route>
-              <Route path="/Todo" exact>
-                <Todo />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
+      
       </div>
     );
 }
