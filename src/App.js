cd1414a2df158a77d0646ea9b51c7a0943f85dd2
Jibrilos <webxite23@gmail.com>
2021-04-12 12:39:15 +0300

    How to install a package

--- src/App.js
+++ src/App.js
@@ -1,37 +1,112 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import "./App.css";
+
+// function App() {
+//   const [users, setUsers] = useState([]);
+//   const [search,  setSearch]  =useState("");
+//   const [isLoading,setIsloading]=useState(false)
+//   const fetchData =  () => {
+//      setIsloading(true);
+//     fetch(`https://api.github.com/search/repositories?q=${search}`)
+//       .then((response) => {
+
+//        return  response.json()})
+//       .then((resData) => {
+//         return setUsers(resData.items)
+
+//       });
+
+//       setIsloading(false);
+
+//   };
+
+//   // useEffect(() => {
+//   // fetchData();
+//   // }, []);
+//   if(isLoading) return <h1>Loading</h1>
+
+//   return (
+//     <div className="App">
+//       <h1>Repository</h1>
+//       <div>
+//         <input
+//           type="text"
+//           onChange={(e) => setSearch(e.target.value)}
+//           value={search}
+//         />
+//         <button onClick={fetchData}>search</button>
+//       </div>
+
+//         <table>
+//           <tbody>
+//             <tr className="cen">
+//               <td>Name</td>
+//               <td>URL</td>
+//             </tr>
+//             {users.map((user, index) => (
+//               <tr key={index} className="wrapper">
+//                 <td className="full_name">{user.full_name}</td>
+//                 <td className="url">{user.url}</td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+
+//     </div>
+//   );
+// }
+
+import React, { useState } from "react";
 import "./App.css";
 
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
+
 function App() {
-  const [users, setUsers] = useState([]);
-  const search="python"
-const fetchData=()=>{
-  fetch(`https://api.github.com/search/repositories?q=${search}`)
-    .then((response) => response.json())
-    .then((resData) => setUsers(resData.items));
+  const [todo, setTodo] = useState({ description: "", date: "", status: "" });
+  const [todos, setTodos] = useState([]);
 
-}
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
 
-  useEffect(() => {
-  fetchData();
-  }, []);
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+    setTodo({ description: "", date: "", status: "" });
+  };
 
   return (
     <div className="App">
-      <h1>Repository</h1>
-      <table>
-        <tbody>
-          <tr className="cen">
-            <td>Name</td>
-            <td>URL</td>
-          </tr>
-          {users.map((user, index) => (
-            <tr key={index} className="wrapper">
-                <td className="full_name">{user.full_name}</td>
-                <td className="url">{user.url}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <input
+        placeholder="Description"
+        name="description"
+        value={todo.description}
+        onChange={inputChanged}
+      />
+      <input
+        placeholder="Date"
+        name="date"
+        value={todo.date}
+        onChange={inputChanged}
+      />
+      <input
+        placeholder="Status"
+        name="status"
+        value={todo.status}
+        onChange={inputChanged}
+      />
+      <button onClick={addTodo}>Add</button>
+      <div
+        className="ag-theme-material"
+        style={{ height: 600, width: 600, margin: "auto" }}
+      >
+        <AgGridReact rowData={todos}>
+          <AgGridColumn field="description"></AgGridColumn>
+          <AgGridColumn field="date"></AgGridColumn>
+          <AgGridColumn field="status"></AgGridColumn>
+        </AgGridReact>
+      </div>
     </div>
   );
 }
