7ea02580142060e0b9eabfbd4c1eff3d0d284907
Jibrilos <webxite23@gmail.com>
2021-04-12 22:30:37 +0300

    add a custom button

--- src/App.js
+++ src/App.js
@@ -1,62 +1,6 @@
-// import React, { useEffect, useState } from "react";
-// import "./App.css";
-
-// function App() {
-//   const [users, setUsers] = useState([]);
-//   const [search,  setSearch]  =useState("");
-//   const [isLoading,setIsloading]=useState(false)
-//   const fetchData =  () => {
-//      setIsloading(true);
-//     fetch(`https://api.github.com/search/repositories?q=${search}`)
-//       .then((response) => {
-
-//        return  response.json()})
-//       .then((resData) => {
-//         return setUsers(resData.items)
-
-//       });
-
-//       setIsloading(false);
-
-//   };
-
-//   // useEffect(() => {
-//   // fetchData();
-//   // }, []);
-//   if(isLoading) return <h1>Loading</h1>
-
-//   return (
-//     <div className="App">
-//       <h1>Repository</h1>
-//       <div>
-//         <input
-//           type="text"
-//           onChange={(e) => setSearch(e.target.value)}
-//           value={search}
-//         />
-//         <button onClick={fetchData}>search</button>
-//       </div>
-
-//         <table>
-//           <tbody>
-//             <tr className="cen">
-//               <td>Name</td>
-//               <td>URL</td>
-//             </tr>
-//             {users.map((user, index) => (
-//               <tr key={index} className="wrapper">
-//                 <td className="full_name">{user.full_name}</td>
-//                 <td className="url">{user.url}</td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-
-//     </div>
-//   );
-// }
-
 import React, { useState } from "react";
+import ReactiveButton from "reactive-button";
+
 import "./App.css";
 
 import { AgGridColumn, AgGridReact } from "ag-grid-react";
@@ -66,6 +10,14 @@ import "ag-grid-community/dist/styles/ag-theme-material.css";
 function App() {
   const [todo, setTodo] = useState({ description: "", date: "", status: "" });
   const [todos, setTodos] = useState([]);
+  const [state, setState] = useState("idle");
+   const onClickHandler = () => {
+     setState("loading");
+     setTimeout(() => {
+       setState("success");
+       addTodo()
+     }, 2000);
+   };
 
   const inputChanged = (event) => {
     setTodo({ ...todo, [event.target.name]: event.target.value });
@@ -96,7 +48,16 @@ function App() {
         value={todo.status}
         onChange={inputChanged}
       />
-      <button onClick={addTodo}>Add</button>
+      {/* <button onClick={addTodo}>Add</button> */}
+      <ReactiveButton
+        buttonState={state}
+        onClick={onClickHandler}
+        size="tiny"
+        rounded={true}
+        idleText={
+        " Add"
+        }
+      />
       <div
         className="ag-theme-material"
         style={{ height: 600, width: 600, margin: "auto" }}
