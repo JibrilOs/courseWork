3650397cb3a27175e28581efa53c1cc6a73ffb9e
Jibrilos <webxite23@gmail.com>
2021-04-12 11:05:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const search="python"
+const fetchData=()=>{
+  fetch(`https://api.github.com/search/repositories?q=${search}`)
+    .then((response) => response.json())
+    .then((resData) => setUsers(resData.items));
+
+}
+
+  useEffect(() => {
+  fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Repository</h1>
+      <table>
+        <tbody>
+          <tr className="cen">
+            <td>Name</td>
+            <td>URL</td>
+          </tr>
+          {users.map((user, index) => (
+            <tr key={index} className="wrapper">
+                <td className="full_name">{user.full_name}</td>
+                <td className="url">{user.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
