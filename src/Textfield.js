51df39efb4c09cd8799aa14002181bd37ca9e46a
Jibrilos <webxite23@gmail.com>
2021-04-12 23:34:41 +0300

    deleted material ui custome style

--- src/Textfield.js
+++ src/Textfield.js
@@ -2,21 +2,14 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch",
-    },
-  },
-}));
+
 
 export default function Textfield({ label }) {
-  const classes = useStyles();
-
   return (
-    (
-    <TextField id="standard-basic" label={label}  style={{marginLeft:"20px"}} />
-  )
+    <TextField
+      id="standard-basic"
+      label={label}
+      style={{ marginLeft: "20px" }}
+    />
   );
 }
